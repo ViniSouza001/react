458b1fe5a49747382dcf812981495d5313c82b2c
ViniSouza001 <viniciussouza130705@gmail.com>
2023-07-20 17:35:01 -0300

    jsx #03

--- my_project/src/App.js
+++ my_project/src/App.js
@@ -1,13 +1,29 @@
-// import logo from './logo.svg';
+// essa parte fora da função servirá apenas para importar componentes
+
 import './App.css';
 
 function App() {
+
+  // aqui dentro da função ficará todo o código e a lógica
+  const idade = 31
+  var nome = "Vinicius"
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/180"
+
+
   return (
     <div className='App'>
       <h1>Olá React!</h1>
-      <p>Meu primeiro App!</p>
+      <p>Seja bem vindo {nome}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
 
+// aqui embaixo será apenas a exportação
 export default App;
