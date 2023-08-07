6827fa802b0a8e13a325bef7f24e40ffdfda51e8
ViniSouza001 <viniciussouza130705@gmail.com>
2023-08-07 16:30:58 -0300

    conditional rendering

--- my_project/src/App.js
+++ my_project/src/App.js
@@ -3,6 +3,7 @@
 import './App.css';
 import Evento from './components/Evento'
 import Form from './components/Form'
+import Condicional from './components/Condicional';
 
 function App () {
   // aqui dentro da função ficará todo o código e a lógica
@@ -11,9 +12,8 @@ function App () {
 
   return (
     <div className='App'>
-      <h1>Testando Eventos</h1>
-      <Evento />
-      <Form />
+      <h1>Renderização condicional</h1>
+      <Condicional />
     </div>
   );
 }
