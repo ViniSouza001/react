5d8d805c25cf981d4146a2acf18c2229712ab063
ViniSouza001 <viniciussouza130705@gmail.com>
2023-08-27 12:45:11 -0300

    Criando a Home #20

--- costs/src/components/layouts/Footer.js
+++ costs/src/components/layouts/Footer.js
@@ -6,17 +6,17 @@ function Footer () {
       <footer className={styles.footer}>
          <ul className={styles.social_list}>
             <li>
-               <a target='_blank' href='https://github.com/vinisouza001'>
+               <a target='_blank' href='https://github.com/vinisouza001' rel='noreferrer'>
                   <FaGithub />
                </a>
             </li>
             <li>
-               <a href='https://www.instagram.com/vinisouza.exe/' target='_blank'>
+               <a href='https://www.instagram.com/vinisouza.exe/' target='_blank' rel='noreferrer'>
                   <FaInstagram />
                </a>
             </li>
             <li>
-               <a href='https://api.whatsapp.com/send/?phone=5519996898117&text&type=phone_number&app_absent=0' target='_blank'>
+               <a href='https://api.whatsapp.com/send/?phone=5519996898117&text&type=phone_number&app_absent=0' rel='noreferrer' target='_blank'>
                   <FaWhatsapp />
                </a>
             </li>
