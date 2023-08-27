0b1e1c0a0a526861513be0891447adcdaa88b700
ViniSouza001 <viniciussouza130705@gmail.com>
2023-08-26 22:51:12 -0300

    navbar and footer #19

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,26 +3,26 @@ import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+
 import Container from './components/layouts/Container'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+import Projects from './components/pages/Projects'
 
 function App () {
   return (
     <Router>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresas</Link>
-        <Link to="/contacts">Contatos</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </header>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/company" element={<Company />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/contacts" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
---
+++ costs/src/components/layouts/Footer.js
@@ -0,0 +1,31 @@
+import { FaInstagram, FaWhatsapp, FaGithub } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
+function Footer () {
+   return (
+      <footer className={styles.footer}>
+         <ul className={styles.social_list}>
+            <li>
+               <a target='_blank' href='https://github.com/vinisouza001'>
+                  <FaGithub />
+               </a>
+            </li>
+            <li>
+               <a href='https://www.instagram.com/vinisouza.exe/' target='_blank'>
+                  <FaInstagram />
+               </a>
+            </li>
+            <li>
+               <a href='https://api.whatsapp.com/send/?phone=5519996898117&text&type=phone_number&app_absent=0' target='_blank'>
+                  <FaWhatsapp />
+               </a>
+            </li>
+         </ul>
+         <p className={styles.copy_right}>
+            <span>Costs</span> &copy; 2021
+         </p>
+      </footer>
+   )
+}
+
+export default Footer
